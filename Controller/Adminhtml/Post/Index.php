d619b660ac6c75f1d1935333d83794868bcf1b7e
igork <igork@mirasvit.com>
2023-12-26 23:26:09 +0200

    ui listing

---
+++ Controller/Adminhtml/Post/Index.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Igor\Blog\Controller\Adminhtml\Post;
+
+use Magento\Backend\App\Action;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\View\Result\Page;
+
+class Index extends Action implements HttpGetActionInterface
+{
+
+    public function execute(): Page
+    {
+        /**
+         * @var Page $resultPage
+         */
+        $resultPage = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+        $resultPage->setActiveMenu('Igor_Blog::post');
+        $resultPage->getConfig()->getTitle()->prepend(__('Manage Posts'));
+
+        return $resultPage;
+    }
+}
